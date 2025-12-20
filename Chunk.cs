841325cfa3db08ced705c5b7cb77ff422f7d455b
Bober <towermanxxl@gmail.com>
2025-12-20 21:20:06 +0100

    working noise world gen

--- Chunk.cs
+++ Chunk.cs
@@ -1,24 +1,43 @@
 
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Godot;
-
+[Tool]
 public partial class Chunk : MeshInstance3D
 {
 	List<Godot.Vector3> vertices = new List<Godot.Vector3>();
 
 	// List< GameObject > gameObjects;
 	List< WorldTile > worldTiles = new List<WorldTile>();
+	FastNoiseLite noise = new Godot.FastNoiseLite();
 	public override void _Ready()
 	{
-		
-		worldTiles.Add(new WorldTile(new Godot.Vector3(0.5f,0.0f,0.5f), 1.0f));
-		worldTiles.Add(new WorldTile(new Godot.Vector3(-0.5f,0.0f,0.5f), 1.0f));
-		worldTiles.Add(new WorldTile(new Godot.Vector3(0.5f,0.0f,-0.5f), 1.0f));
-		worldTiles.Add(new WorldTile(new Godot.Vector3(-0.5f,0.0f,-0.5f), 1.0f));
-		
+		int startXpos = -16;
+		int startZpos = -16;
 
+		for (int i = 0; i<32; i++)
+		{
+			for (int j = 0; j<32; j++)
+			{
+				float x = i + startXpos;
+				float z = j + startZpos;
+				float y = noise.GetNoise2D(x,z);
+				// y is in -0.5 to 0.5
+				y += 0.5f; // move it to 0-1
+
+				y *= 50; // move it to 0-50
+				// now y is a float which we don't like for our world so we put it in 0-1-2-3..-50range for tiling
+				y = (int)y;
+				worldTiles.Add(new WorldTile(new Godot.Vector3(x,y,z), 1.0f));
+			}	
+		}
+
+		
+		
+		
+	
 		GenMesh();
 
 
