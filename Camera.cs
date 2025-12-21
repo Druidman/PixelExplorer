dac0d29fce83c94c3a6ff6e3fb879a165e28fd03
Bober <towermanxxl@gmail.com>
2025-12-21 20:19:41 +0100

    moved camera

--- Camera.cs
+++ Camera.cs
@@ -3,19 +3,13 @@ using Godot;
 
 public partial class Camera : Camera3D
 {
-	float angle = 90.0f;
-	Vector3 startPosition;
-	// Called when the node enters the scene tree for the first time.
+
+
+
 	public override void _Ready()
 	{
-		startPosition = GlobalPosition;
+		CharacterBody3D player = (CharacterBody3D)GetParent();
+		LookAt(player.GlobalPosition);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		RotateY(Mathf.DegToRad(angle * (float)delta));
-		GlobalPosition = startPosition.Rotated(Vector3.Up, Rotation.Y);
-
-	}
 }
