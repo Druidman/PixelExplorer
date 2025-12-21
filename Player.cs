c3008b5afdfe7313a1b4ba72ac31ae7f5cbba95f
Bober <towermanxxl@gmail.com>
2025-12-21 20:13:44 +0100

    added proper movement

--- Player.cs
+++ Player.cs
@@ -4,12 +4,25 @@ using System;
 public partial class Player : CharacterBody3D
 {
 	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+	public const float DecelerationSpeed = Speed * 0.1f;
+	public const float JumpForce = 10;
+	public float MouseSensitivity = 0.2f;
+	float GravitySpeed = 20.0f;
+
+    public override void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+    }
 
 	public override void _Input(InputEvent inputEvent)
 	{
-	   if (inputEvent.GetType() == typeof(InputEventMouseMotion))
+	   if (inputEvent is InputEventMouseMotion eventMouseMotion)
 		{
+			int width = DisplayServer.WindowGetSize().X;
+			float change = (float)width / 360.0f;
+
+			float angle = eventMouseMotion.Relative.X * change * this.MouseSensitivity;
+			RotateY(-Mathf.DegToRad(angle));
 			
 		}
 	}
@@ -19,33 +32,63 @@ public partial class Player : CharacterBody3D
 		Vector3 velocity = Velocity;
 
 		// Add the gravity.
-		if (!IsOnFloor())
+		if (IsOnFloor())
 		{
-			velocity += GetGravity() * (float)delta;
-		}
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
+			velocity.Y = 0;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+			velocity.Y -= this.GravitySpeed * (float)delta;
+		}
+		Godot.Vector3 movement = new Godot.Vector3(0.0f,0.0f, 0.0f);
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("move_up") && IsOnFloor())
+		{
+			velocity.Y = JumpForce;
+		}
+
+		
+		if (Input.IsActionPressed("move_forward"))
+		{
+			movement.X += 1.0f;
+		}
+		if (Input.IsActionPressed("move_backward"))
+		{
+			movement.X += -1.0f;
+		}
+		if (Input.IsActionPressed("move_left"))
+		{
+			movement.Z += -1.0f;
+		}
+		if (Input.IsActionPressed("move_right"))
+		{
+			movement.Z += 1.0f;
+		}
+		movement *= Speed;
+
+		movement = movement.Rotated(Godot.Vector3.Up, this.Rotation.Y + Mathf.DegToRad(90));
+		if (movement.Z != 0.0f)
+		{
+			velocity.Z = movement.Z;
+		}
+		else
+		{
+			velocity.Z = Mathf.MoveToward(velocity.Z, 0, DecelerationSpeed);
+		}
+
+		if (movement.X != 0.0f)
+		{
+			velocity.X = movement.X;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(velocity.X, 0, DecelerationSpeed);
 		}
 
 		Velocity = velocity;
+		
 		MoveAndSlide();
+
 	}
 }
