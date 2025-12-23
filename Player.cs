ac06f0785a7ec84c43136d3fa4f507da9157251b
Bober <towermanxxl@gmail.com>
2025-12-23 21:30:42 +0100

    implemented mouse player control BUT without path finding yet

--- Player.cs
+++ Player.cs
@@ -9,89 +9,65 @@ public partial class Player : CharacterBody3D
 	public const float JumpForce = 10;
 	public float MouseSensitivity = 0.2f;
 	float GravitySpeed = 20.0f;
+	MeshInstance3D character;
+	CollisionShape3D characterCollider;
+	Camera camera;
 
-    public override void _Ready()
-    {
-        Input.MouseMode = Input.MouseModeEnum.Captured;
-    }
+	Godot.Vector3 destiny;
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Confined;
+		this.character = (MeshInstance3D)GetNode("Character");
+		this.characterCollider = (CollisionShape3D)GetNode("CharacterCollider");
+		this.camera = (Camera)GetNode("Camera3D");
+
+		destiny = this.Position;
+
+		
+	}
 
 	public override void _Input(InputEvent inputEvent)
 	{
-	   if (inputEvent is InputEventMouseMotion eventMouseMotion)
-		{
-			int width = DisplayServer.WindowGetSize().X;
-			float change = (float)width / 360.0f;
-
-			float angle = eventMouseMotion.Relative.X * change * this.MouseSensitivity;
-			RotateY(-Mathf.DegToRad(angle));
-			
-		}
 		if (inputEvent.IsActionPressed("exit"))
 		{
 			Input.MouseMode = Input.MouseModeEnum.Visible;
+		}
+		if (inputEvent.IsPressed() && inputEvent is InputEventMouseButton)
+		{
+			
+			Godot.Vector2 mousePos = GetViewport().GetMousePosition(); 
+
+			Godot.Vector3 origin = this.camera.ProjectRayOrigin(mousePos);
+			Godot.Vector3 end = origin + this.camera.ProjectRayNormal(mousePos) * 1000;
+
+
+			PhysicsDirectSpaceState3D space_state = this.GetWorld3D().DirectSpaceState;
+			PhysicsRayQueryParameters3D query = PhysicsRayQueryParameters3D.Create(origin, end);
+			var result = space_state.IntersectRay(query);
+			if (result.Count > 0)
+			{
+				var hitPos = (Godot.Vector3)result["position"];	
+
+				var angle = new Vector2(this.Position.X, this.Position.Z).AngleToPoint(new Vector2(hitPos.X, hitPos.Z));
+				this.character.RotateY(-(angle + this.character.Rotation.Y));
+				this.characterCollider.RotateY(-(angle + this.character.Rotation.Y));
+				
+
+				
+
+
+				this.destiny = hitPos;
+				
+				
+				
+			}
+			
 		}
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector3 velocity = Velocity;
-
-		// Add the gravity.
-		if (IsOnFloor())
-		{
-			velocity.Y = 0;
-		}
-		else
-		{
-			velocity.Y -= this.GravitySpeed * (float)delta;
-		}
-		Godot.Vector3 movement = new Godot.Vector3(0.0f,0.0f, 0.0f);
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("move_up") && IsOnFloor())
-		{
-			velocity.Y = JumpForce;
-		}
-
-		
-		if (Input.IsActionPressed("move_forward"))
-		{
-			movement.X += 1.0f;
-		}
-		if (Input.IsActionPressed("move_backward"))
-		{
-			movement.X += -1.0f;
-		}
-		if (Input.IsActionPressed("move_left"))
-		{
-			movement.Z += -1.0f;
-		}
-		if (Input.IsActionPressed("move_right"))
-		{
-			movement.Z += 1.0f;
-		}
-		movement *= Speed;
-
-		movement = movement.Rotated(Godot.Vector3.Up, this.Rotation.Y + Mathf.DegToRad(90));
-		if (movement.Z != 0.0f)
-		{
-			velocity.Z = movement.Z;
-		}
-		else
-		{
-			velocity.Z = Mathf.MoveToward(velocity.Z, 0, DecelerationSpeed);
-		}
-
-		if (movement.X != 0.0f)
-		{
-			velocity.X = movement.X;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(velocity.X, 0, DecelerationSpeed);
-		}
-
-		Velocity = velocity;
+		// Implement path finder
 		
 		MoveAndSlide();
 
