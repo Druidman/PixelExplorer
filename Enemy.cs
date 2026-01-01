56fc6171ce7705616d9341a414540a787f73dab1
Bober <towermanxxl@gmail.com>
2026-01-01 06:44:57 +0100

    got working enemy spawner

--- Enemy.cs
+++ Enemy.cs
@@ -1,15 +1,29 @@
 using Godot;
 using System;
 
-public partial class Enemy : CharactrerBody3D
+public partial class Enemy : CharacterBody3D
 {
+
+	float GravitySpeed = 20.0f;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
+		Godot.Vector3 velocity = Velocity;
+		if (IsOnFloor())
+		{
+			velocity.Y = 0;
+		}
+		else
+		{
+			velocity.Y -= this.GravitySpeed * (float)delta;
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
 	}
 }
