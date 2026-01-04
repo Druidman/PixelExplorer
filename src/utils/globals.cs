75395ff0e8728364d2becac5eca01f93887722a4
Bober <towermanxxl@gmail.com>
2026-01-04 11:25:26 +0100

    turn off debug

--- src/utils/globals.cs
+++ src/utils/globals.cs
@@ -6,7 +6,7 @@ class GameGlobals
 {
 
     public static int ChunkCoinLimit = 1;
-    public static bool DebugMode = true;
+    public static bool DebugMode = false;
     public static int ChunkWidth = 16;
     public static Godot.Vector3 StartWorldMiddle = new Godot.Vector3(0,0,0);
     public static int chunkRadius = 10;
