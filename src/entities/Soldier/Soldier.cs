a2b42b3006b825e5a674c66dc340956e77ab8191
Bober <towermanxxl@gmail.com>
2026-01-04 22:26:19 +0100

    not fully tested but working soldiers

--- src/entities/Soldier/Soldier.cs
+++ src/entities/Soldier/Soldier.cs
@@ -1,10 +1,25 @@
+using System.Diagnostics;
 using Godot;
 
-public partial class Soldier : CharacterBody3D
+public partial class Soldier : Node3D
 {
+
+	[Export]
+	RayCast3D BottomRayCast;
+	[Export]
+	RayCast3D LeftRayCast;
+	[Export]
+	RayCast3D RightRayCast;
 	private Player player;
 
 	private Godot.Vector3 relativeToPlayer;
+	Godot.Vector3 destination;
+	Godot.Vector3 move;
+
+	Godot.Vector3 velocity;
+	float GroundCheckOffset = 0.1f;
+
+	Godot.Vector3 rotationOffset = new Godot.Vector3(0,Mathf.DegToRad(90),0);
 	
 	private float stopDistance = 0.2f;
 
@@ -16,14 +31,17 @@ public partial class Soldier : CharacterBody3D
 	}
 	public override void _Ready()
 	{
-		this.GlobalPosition = this.player.GlobalPosition;
+		this.GlobalPosition = this.player.GlobalPosition + new Godot.Vector3(0,10,0);
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Godot.Vector3 velocity = Velocity;
+		this.Rotation = this.player.characterCollider.Rotation - rotationOffset;
 		
-		if (this.IsOnFloor() )
+		this.BottomRayCast.ForceRaycastUpdate();
+		bool isGroundUnder = this.BottomRayCast.IsColliding();
+
+		if (isGroundUnder)
 		{
 			velocity.Y = 0;
 		}
@@ -31,34 +49,49 @@ public partial class Soldier : CharacterBody3D
 		{
 			velocity.Y -= GameGlobals.GravitySpeed * (float)delta;
 		}
+		
+
+		
 
 		Godot.Vector3 destination = this.player.GlobalPosition + this.relativeToPlayer;
-		Godot.Vector3 move = destination - this.GlobalPosition;
-		move = move.Normalized();
+		Godot.Vector3 direction = (destination - this.GlobalPosition).Normalized();
 
-
-		if ((this.GlobalPosition - destination).LengthSquared() > this.stopDistance * this.stopDistance)
+		if (this.GlobalPosition.DistanceSquaredTo(destination) < this.stopDistance * this.stopDistance)
 		{
-			velocity.X = move.X * GameGlobals.PlayerSpeed;
-
-			velocity.Z = move.Z * GameGlobals.PlayerSpeed;
+			direction *= 0;
 		}
 		else
 		{
-			velocity.X = 0;
-
-			velocity.Z = 0;
+			direction *= GameGlobals.PlayerSpeed;
 		}
+		
 
-		if (IsOnWall())
+		velocity.X = direction.X;
+		velocity.Z = direction.Z;
+
+		this.GlobalPosition += velocity * (float)delta;
+
+
+		if (isGroundUnder && velocity.Y == 0)
 		{
-			velocity.Y += 1;
+			this.GlobalPosition = new Godot.Vector3(this.GlobalPosition.X,this.BottomRayCast.GetCollisionPoint().Y + 0.1f,this.GlobalPosition.Z);
 		}
 
-		Velocity = velocity;
+		this.LeftRayCast.ForceRaycastUpdate();
+		this.RightRayCast.ForceRaycastUpdate();
+
+		bool isWallInFront = this.LeftRayCast.IsColliding() || this.RightRayCast.IsColliding();
 
 		
-		MoveAndSlide();
+
+		if (isWallInFront && (velocity.X != 0 || velocity.Z != 0))
+		{
+			velocity.Y += GameGlobals.PlayerJumpForce * 5;
+			
+		}
+		
+
+
 	}
 
 }
